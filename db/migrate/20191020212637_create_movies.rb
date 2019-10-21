185a75c6220e988ce4aa5e9ecbca9cc6b585e941
Barbara <muesingb@gmail.com>
2019-10-21 13:41:53 -0400

    Done.

---
+++ db/migrate/20191020212637_create_movies.rb
@@ -0,0 +1,17 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
+  end
+end
+
+# title	(string)
+# release_date	(integer)
+# director	(string)
+# lead	(string)
+# in_theaters	(boolean)
